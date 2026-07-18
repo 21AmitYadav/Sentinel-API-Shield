4459224d6f681bc8184ac02e4ec62a7a51131a92
Amit Yadav <16yadavamit@gmail.com>
2026-07-19 00:00:02 +0530

    Complete Sprint 1: JWT authentication and protected routes

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -7,11 +7,18 @@ from app.models.user import User
 from app.repositories.user_repository import UserRepository
 from app.security.jwt_handler import decode_access_token
 
-outh2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 
-def get_current_user(token:str=Depends(outh2_scheme),db:Session = Depends(get_db)) -> User:
-    user_id = decode_access_token(token)
+def get_current_user(token:str=Depends(oauth2_scheme),db:Session = Depends(get_db)) -> User:
+    try:
+        user_id = decode_access_token(token)
 
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
     
     user_repository = UserRepository(db)
@@ -19,10 +26,10 @@ def get_current_user(token:str=Depends(outh2_scheme),db:Session = Depends(get_db
 
     if user is None:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User not found",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     
     return user
 
